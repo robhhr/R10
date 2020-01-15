b7cb914ba1493628235da136e8e25a5197d19dd3
Roberto H <robhhr@outlook.com>
2020-01-15 01:58:52 -0800

    enable toggable text and organize fetched data

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   bracketSpacing: false,
   jsxBracketSameLine: true,
+  semi: false,
   singleQuote: true,
+  tabWidth: 3,
   trailingComma: 'all',
-};
+}
--- components/pages/about/About.js
+++ components/pages/about/About.js
@@ -1,36 +1,68 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-import {gql} from 'apollo-boost';
-import {useQuery} from '@apollo/react-hooks';
+import React, {useState} from 'react'
+import {
+   View,
+   Text,
+   StyleSheet,
+   SafeAreaView,
+   ScrollView,
+   TouchableOpacity,
+} from 'react-native'
+import {gql} from 'apollo-boost'
+import {useQuery} from '@apollo/react-hooks'
 
 const About = () => {
-  const EVENT_INFORMATION = gql`
-    {
-      allConducts {
-        id
-        description
-        title
-        order
+   const EVENT_INFORMATION = gql`
+      {
+         allConducts {
+            id
+            description
+            title
+            order
+         }
       }
-    }
-  `;
+   `
 
-  const {loading, error, data} = useQuery(EVENT_INFORMATION);
+   const {loading, error, data} = useQuery(EVENT_INFORMATION)
 
-  return loading ? (
-    <Text>nada</Text>
-  ) : error ? (
-    <Text>error</Text>
-  ) : (
-    <View>
-      {data.allConducts.map(({title, description}) => (
-        <View key={title}>
-          <Text>{title}</Text>
-          <Text>{description}</Text>
-        </View>
-      ))}
-    </View>
-  );
-};
+   const [toggle, setToggle] = useState(false)
 
-export default About;
+   const triggerToggle = () => {
+      setToggle(toggle === false)
+   }
+
+   return loading ? (
+      <SafeAreaView style={styles.container}>
+         <Text>loading</Text>
+      </SafeAreaView>
+   ) : error ? (
+      <SafeAreaView style={styles.container}>
+         <Text>error</Text>
+      </SafeAreaView>
+   ) : (
+      <SafeAreaView style={styles.container}>
+         <ScrollView style={styles.scrollView}>
+            <View>
+               {data.allConducts.map(({id, title, description}) => (
+                  <View key={id}>
+                     <TouchableOpacity onPress={triggerToggle}>
+                        <Text>{title}</Text>
+                     </TouchableOpacity>
+                     {toggle ? <Text>{description}</Text> : null}
+                  </View>
+               ))}
+            </View>
+         </ScrollView>
+      </SafeAreaView>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+   },
+   scrollView: {
+      backgroundColor: 'pink',
+   },
+})
+
+export default About
