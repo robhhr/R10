78e7faeba03d8e3b2b406817b25f8dc8538c35c0
Roberto H <robhhr@outlook.com>
2020-01-16 20:36:08 -0800

    fix: clean up unused code

--- components/App.js
+++ components/App.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { About } from './pages/index'
 import ApolloWrapper from '../apollo/Apollo'
 import Routes from '../navigation/Routes'
 
@@ -11,7 +9,5 @@ const App = () => {
     </ApolloWrapper>
   )
 }
-
-const styles = StyleSheet.create({})
 
 export default App
