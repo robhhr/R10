8353c24f265bc55fd34803c72e0048b53dc648d1
Roberto H <robhhr@outlook.com>
2020-01-16 20:35:17 -0800

    feat: bottom tab navigation

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
+import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { About, Schedule, Favorites } from '../components/pages'
 
-const AppNavigator = createStackNavigator({
-  About,
-  Schedule,
+const HomeRoute = createStackNavigator({
+  Home: Schedule,
 })
 
-export default createAppContainer(AppNavigator)
+const AboutRoute = createStackNavigator({
+  About: About,
+})
+
+const FavRoute = createStackNavigator({
+  Favorites: Favorites,
+})
+
+export default createAppContainer(
+  createBottomTabNavigator({
+    Home: HomeRoute,
+    About: AboutRoute,
+    Favorites: FavRoute,
+  }),
+)
