d6ad1db708b6bc3d467bee26553dfb9137a11ef4
Roberto H <robhhr@outlook.com>
2020-01-16 21:22:42 -0800

    feat: enable components and styles for bottom navigation

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { Image } from 'react-native'
 import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
-import { About, Schedule, Favorites } from '../components/pages'
+import { About, Schedule, Favorites, Map } from '../components/pages'
 
 const HomeRoute = createStackNavigator({
   Home: Schedule,
@@ -16,10 +17,45 @@ const FavRoute = createStackNavigator({
   Favorites: Favorites,
 })
 
-export default createAppContainer(
-  createBottomTabNavigator({
-    Home: HomeRoute,
-    About: AboutRoute,
+const MapRoute = createStackNavigator({
+  Map: Map,
+})
+
+const BottomNavigation = createBottomTabNavigator(
+  {
+    Schedule: HomeRoute,
+    Map: MapRoute,
     Favorites: FavRoute,
-  }),
+    About: AboutRoute,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state
+        if (routeName === 'Home') {
+          return (
+            <Image
+              source={require('../assets/images/R2D2.png')}
+              style={{ width: 20, height: 20 }}
+            />
+          )
+        } else {
+          return (
+            <Image
+              source={require('../assets/images/R2D2.png')}
+              style={{ width: 20, height: 20 }}
+            />
+          )
+        }
+      },
+    }),
+    tabBarOptions: {
+      activeBackgroundColor: 'black',
+      inactiveBackgroundColor: 'black',
+      activeTintColor: 'white',
+      inactiveTintColor: '#999999',
+    },
+  },
 )
+
+export default createAppContainer(BottomNavigation)
