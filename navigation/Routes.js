c5e67b2a61ce1afb6e5c297f4d90fc8df9acca1c
Roberto H <robhhr@outlook.com>
2020-01-17 21:57:07 -0800

    add correct icons to bottom nav

--- navigation/Routes.js
+++ navigation/Routes.js
@@ -4,6 +4,7 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { About, Schedule, Favorites, Map } from '../components/pages'
+import Icon from 'react-native-vector-icons/Ionicons'
 
 const HomeRoute = createStackNavigator({
   Home: Schedule,
@@ -30,36 +31,18 @@ const BottomNavigation = createBottomTabNavigator(
   },
   {
     defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: () => {
+      tabBarIcon: ({ tintColor }) => {
         const { routeName } = navigation.state
 
         if (routeName === 'Schedule') {
-          return (
-            <Image
-              source={require('../assets/images/R2D2.png')}
-              style={{ width: 30, height: 30 }}
-            />
-          )
+          return <Icon name="ios-calendar" color={tintColor} size={25} />
         } else if (routeName === 'Map') {
-          return (
-            <Image
-              source={require('../assets/images/C3P0.png')}
-              style={{ width: 30, height: 30 }}
-            />
-          )
+          return <Icon name="ios-map" color={tintColor} size={25} />
         } else if (routeName === 'Favorites') {
-          return (
-            <Image
-              source={require('../assets/images/Rebel.png')}
-              style={{ width: 30, height: 30 }}
-            />
-          )
+          return <Icon name="ios-heart" color={tintColor} size={25} />
         } else if (routeName === 'About') {
           return (
-            <Image
-              source={require('../assets/images/Yoda.png')}
-              style={{ width: 30, height: 30 }}
-            />
+            <Icon name="ios-information-circle" color={tintColor} size={25} />
           )
         }
       },
@@ -68,7 +51,7 @@ const BottomNavigation = createBottomTabNavigator(
       activeTintColor: 'white',
       inactiveTintColor: '#999999',
       style: { height: 65, backgroundColor: 'black' },
-      labelStyle: { fontSize: 10 },
+      labelStyle: { fontSize: 12 },
     },
   },
 )
