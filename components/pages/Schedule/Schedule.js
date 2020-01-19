93e1f4f4b1f744adc745953b31933a730659608a
Roberto H <robhhr@outlook.com>
2020-01-18 16:32:44 -0800

    feat: get individual session w/ start time

--- components/pages/Schedule/Schedule.js
+++ components/pages/Schedule/Schedule.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, SectionList } from 'react-native'
 import { gql } from 'apollo-boost'
 import { useQuery } from '@apollo/react-hooks'
 import { Header, Logo, Wrapper } from '../../index'
@@ -24,6 +24,25 @@ const Conference = () => {
     }
   `
 
+  const reduceSessionsToHeaders = (headers, session) => {
+    const sectionIndex = headers.findIndex(
+      ({ time }) => time === session.startTime,
+    )
+
+    if (sectionIndex === -1)
+      return [
+        ...headers,
+        {
+          time: session.startTime,
+          data: [session],
+        },
+      ]
+
+    headers[sectionIndex].data.push(session)
+
+    return headers
+  }
+
   const { loading, error, data } = useQuery(EVENT_INFORMATION)
   return (
     <ScrollView>
@@ -33,17 +52,25 @@ const Conference = () => {
         <Title>error</Title>
       ) : (
         <View>
-          {data.allSessions.map(({ id, startTime, title, location }) => (
-            <Text
-              key={id}
-              title={title}
-              style={{
-                color: 'black',
-                backgroundColor: 'grey',
-              }}>
-              {title} {location}
-            </Text>
-          ))}
+          <SectionList
+            sections={data.allSessions.reduce(reduceSessionsToHeaders, [])}
+            keyExtractor={({ id }) => id}
+            renderItem={({ item: { id, title, location } }, i) => (
+              <View>
+                <Text
+                  key={id}
+                  title={title}
+                  style={{
+                    color: 'black',
+                    backgroundColor: 'grey',
+                    borderWidth: 2,
+                  }}>
+                  {title} {location}
+                </Text>
+              </View>
+            )}
+            renderSectionHeader={({ section: { time } }) => <Text>{time}</Text>}
+          />
         </View>
       )}
     </ScrollView>
