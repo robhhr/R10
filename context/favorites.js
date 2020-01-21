02d868b8a36474d2d3fbcd723726832c73c1c3e1
Roberto H <robhhr@outlook.com>
2020-01-21 01:05:03 -0800

    fix: refactor favs context to update fav list

--- context/favorites.js
+++ context/favorites.js
@@ -1,30 +1,70 @@
+import React, { createContext, useState, useEffect } from 'react'
 import AsyncStorage from '@react-native-community/async-storage'
 
-const storeFavorites = async id => {
-  try {
-    await AsyncStorage.setItem(
-      `${id}`,
-      JSON.stringify({ id: id, faved: new Date() }),
-    )
-  } catch (e) {
-    console.error('store bloop')
+const FavoritesContext = createContext()
+
+const FavoritesContextProvider = () => {
+  const [fav, setFav] = useState([])
+
+  const storeFavorites = async id => {
+    try {
+      await AsyncStorage.setItem('fav', JSON.stringify({ ...fav, id }))
+    } catch (e) {
+      console.error('store bloop')
+    }
+    getFavs()
   }
+
+  const getFavorites = async () => {
+    try {
+      const fav = await AsyncStorage.getItem('fav')
+      return JSON.parse(fav)
+    } catch (e) {
+      console.error('read blop')
+    }
+  }
+
+  const deleteFavorites = id => {
+    try {
+      AsyncStorage.removeItem(`${id}`)
+    } catch (e) {
+      console.error('delete bloop')
+    }
+    getFavs()
+  }
+
+  const getFavs = async () => {
+    const selectedFavorites = await getFavorites()
+    // const id = selectedFavorites.map(fav => fav[0])
+    setFav(selectedFavorites)
+  }
+
+  // const addFavs = async (favid) => {
+  //   try {
+  //     const newFavorite = await storeFavorites(favid)
+  //     setId({
+  //       id: [...id, newFavorite]
+  //     })
+  //     getFavs()
+  //   } catch (e) {
+  //     console.error('add bloop')
+  //   }
+  // }
+
+  // const removeFavs = async (favid) => {
+  //   try {
+  //     await deleteFavorites(favid)
+  //     getFavs()
+  //   } catch (e) {
+  //     console.error('remove bloop')
+  //   }
+  // }
+
+  useEffect(() => {
+    getFavs()
+  }, [])
+
+  return [fav, storeFavorites, deleteFavorites]
 }
 
-const deleteFavorites = id => {
-  try {
-    AsyncStorage.removeItem(`${id}`)
-  } catch (e) {
-    console.error('delete bloop')
-  }
-}
-
-const getFavorites = async keys => {
-  const keys = await AsyncStorage.getAllKeys()
-  try {
-    const favorites = await AsyncStorage.multiGet(keys)
-    return favorites
-  } catch (e) {
-    console.error('read blop')
-  }
-}
+export default FavoritesContextProvider
