6047390fa01497dbb8ea5f594032978a06218ce4
Roberto H <robhhr@outlook.com>
2020-01-29 01:09:45 -0800

    style button to add/remove favorites

--- components/Button.js
+++ components/Button.js
@@ -1,22 +1,32 @@
 import React, { useContext } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { FavoritesContext } from '../context/favorites'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
+import styles from './Button.styles'
 
 const Button = ({ id, children, ...props }) => {
   const { favorites, addFavs, removeFavs } = useContext(FavoritesContext)
 
   return (
-    <TouchableOpacity
-      onPress={() =>
-        favorites && favorites.includes(id) ? removeFavs(id) : addFavs(id)
-      }>
-      <Text>
-        {favorites && favorites.includes(id)
-          ? 'Remove from Faves'
-          : 'Add to Faves'}
-      </Text>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <LinearGradient
+        colors={['#9963ea', '#8797D6']}
+        start={{ x: 0.0, y: 1.0 }}
+        end={{ x: 1.0, y: 0.0 }}
+        style={styles.button}>
+        <TouchableOpacity
+          onPress={() =>
+            favorites && favorites.includes(id) ? removeFavs(id) : addFavs(id)
+          }>
+          <Text style={styles.buttonText}>
+            {favorites && favorites.includes(id)
+              ? 'Remove from Faves'
+              : 'Add to Faves'}
+          </Text>
+        </TouchableOpacity>
+      </LinearGradient>
+    </View>
   )
 }
 
---
+++ components/Button.styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    marginVertical: 70,
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  button: {
+    width: '75%',
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 22,
+    fontWeight: '600',
+    width: '100%',
+    textAlign: 'center',
+    marginVertical: 15,
+  },
+})
+
+export default styles
