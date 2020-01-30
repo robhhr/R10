8c1346fce71cb28e8751276a8d5c1a2c72c9c155
Roberto H <robhhr@outlook.com>
2020-01-29 18:25:39 -0800

    modal close position

--- components/Modal.js
+++ components/Modal.js
@@ -13,16 +13,54 @@ import { Title } from './Typography'
 const ModalContainer = ({ children, open = false, onClose, ...props }) => {
   return (
     <View>
-      <Modal visible={open} transparent={false} animationType="slide">
-        <SafeAreaView style={{ backgroundColor: 'black', height: '100%' }}>
-          <Text style={{ color: 'white' }}>About the Speaker</Text>
-          <View style={{ backgroundColor: 'white', marginHorizontal: 10 }}>
-            <Title>x</Title>
-            <Title>{children}</Title>
-            <TouchableOpacity onPress={onClose}></TouchableOpacity>
-          </View>
-        </SafeAreaView>
-      </Modal>
+      <ScrollView>
+        <Modal visible={open} transparent={false} animationType="slide">
+          <SafeAreaView
+            style={{
+              backgroundColor: 'black',
+              height: '100%',
+            }}>
+            <View
+              style={{
+                position: 'relative',
+                marginTop: 30,
+                width: '100%',
+              }}>
+              <TouchableOpacity onPress={onClose}>
+                <Text
+                  style={{
+                    color: 'white',
+                    fontSize: 20,
+                    position: 'absolute',
+                    left: 25,
+                    zIndex: 999,
+                  }}>
+                  X
+                </Text>
+              </TouchableOpacity>
+              <Text
+                style={{
+                  color: 'white',
+                  fontWeight: '700',
+                  fontSize: 20,
+                  textAlign: 'center',
+                  zIndex: -999,
+                }}>
+                About the Speaker
+              </Text>
+            </View>
+            <View
+              style={{
+                backgroundColor: 'white',
+                marginHorizontal: 20,
+                marginVertical: 35,
+                borderRadius: 5,
+              }}>
+              <Title>{children}</Title>
+            </View>
+          </SafeAreaView>
+        </Modal>
+      </ScrollView>
     </View>
   )
 }
