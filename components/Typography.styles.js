6232d728c73da0fb4e349fa4811ed4b4173b4c84
Roberto H <robhhr@outlook.com>
2020-02-03 04:13:11 -0800

    add custom fonts

--- components/Typography.styles.js
+++ components/Typography.styles.js
@@ -1,47 +1,57 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 
 const styles = StyleSheet.create({
   author: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 20,
     lineHeight: 35,
     fontWeight: '400',
     marginLeft: 20,
   },
   title: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 28,
   },
   pageTitle: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 28,
     color: 'white',
     paddingVertical: 80,
   },
   sessionTitle: {
+    fontFamily: 'Montserrat-Light',
+
     fontSize: 28,
     marginVertical: 5,
   },
   modalTitle: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 28,
     marginBottom: 30,
     textAlign: 'center',
   },
   aboutText: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 20,
     marginVertical: 25,
     fontWeight: '300',
   },
   sessionText: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 20,
     lineHeight: 35,
     fontWeight: '400',
     marginBottom: 40,
   },
   conduct: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 20,
     fontWeight: '600',
     color: '#9963ea',
     marginVertical: 15,
   },
   redFooter: {
+    fontFamily: 'Montserrat-Light',
     fontSize: 20,
     marginVertical: 25,
     fontWeight: '300',
